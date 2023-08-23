3e188202a2282dc0dd6c601b7a7081d120b3009e
Saturn56 <S.DeepPowder@gmail.com>
2023-08-23 07:27:15 +0200

    try to rewrite ItemBindFeature

--- src/main/java/tools/redstone/redstonetools/features/commands/ItemBindFeature.java
+++ src/main/java/tools/redstone/redstonetools/features/commands/ItemBindFeature.java
@@ -2,6 +2,7 @@ package tools.redstone.redstonetools.features.commands;
 
 
 import com.google.auto.service.AutoService;
+import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.item.ItemStack;
@@ -9,6 +10,7 @@ import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtString;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
+import org.antlr.v4.codegen.model.chunk.RulePropertyRef_ctx;
 import tools.redstone.redstonetools.features.AbstractFeature;
 import tools.redstone.redstonetools.features.Feature;
 import tools.redstone.redstonetools.features.feedback.Feedback;
@@ -19,21 +21,23 @@ import tools.redstone.redstonetools.utils.ItemUtils;
 public class ItemBindFeature extends CommandFeature{
     public static boolean waitingForCommand = false;
     private static ServerPlayerEntity player;
+    private static CommandContext ctx;
 
 
     @Override
-    protected Feedback execute(ServerCommandSource source) throws CommandSyntaxException {
+    protected Feedback execute(CommandContext ctx) throws CommandSyntaxException {
+        this.ctx = ctx;
 
-        player = source.getPlayer();
+        player = ctx.source().player();
         waitingForCommand = true;
 
         return Feedback.success("Please run any command and hold the item you want the command be bound to");
     }
 
     public static Feedback addCommand(String command) {
-        if (!waitingForCommand || MinecraftClient.getInstance().getServer() == null) return null;
+        if (!waitingForCommand || ctx.source().getServer() == null) return null;
 
-        if (player == null || MinecraftClient.getInstance().getServer().getPlayerManager().getPlayer(player.getUuid()) != player) {
+        if (player == null || ctx.source().getPlayer(player.getUuid()) != player) {
             waitingForCommand = false;
             return null;
         }
@@ -43,7 +47,11 @@ public class ItemBindFeature extends CommandFeature{
             return Feedback.error("You need to be holding an item!");
         }
 
-        mainHandStack.getOrCreateNbt().put("command", NbtString.of(command));
+
+        either(
+                () -> mainHandStack.getOrCreateNbt().put("command", NbtString.of(command)),
+                () -> mainHandStack.getOrCreateTag().put("command", NbtString.of(command)) // 1.16 - 1.17
+        );
         ItemUtils.addExtraNBTText(mainHandStack,"Command");
 
         waitingForCommand = false;
